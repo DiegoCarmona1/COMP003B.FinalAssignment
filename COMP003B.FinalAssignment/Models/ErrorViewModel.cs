b9a4f80c93b0a513da1dbb78eb078c077d42a43a
Diego Carmona <ultgojifan@gmail.com>
2024-05-14 20:25:01 -0700

    Add project files.

---
+++ COMP003B.FinalAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.FinalAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
