b678a03065fe140b53f395652c282472f8403d4e
Diego Carmona <ultgojifan@gmail.com>
2024-05-14 23:08:01 -0700

    Added new folder "Data" and added Db Context and Db Sets for all entities

---
+++ COMP003B.FinalAssignment/Data/WebDevAcademyContext.cs
@@ -0,0 +1,22 @@
+﻿using COMP003B.FinalAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.FinalAssignment.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options)
+            : base(options)
+
+        {
+        }
+
+        public DbSet<Creator> Creators { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<DailyMeal> DailyMeals { get; set; }
+        public DbSet<SeasonalMeal> SeasonalMeals { get; set; }
+        public DbSet<HolidayMeal> HolidayMeals { get; set; }
+    }
+}
+}
