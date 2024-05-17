7a3aa419e75160275d6750b3b13a2ac04099c9eb
Diego Carmona <ultgojifan@gmail.com>
2024-05-17 03:05:04 -0700

    Completed APISeasonal controller, proceeding with other API controllers and then configuring to project

--- COMP003B.FinalAssignment/Controllers/APISeasonals.cs
+++ COMP003B.FinalAssignment/Controllers/APISeasonals.cs
@@ -26,13 +26,48 @@ namespace COMP003B.FinalAssignment.Controllers
         [HttpGet("(id)")]
         public ActionResult<Seasonal> GetSeasonalById(int id)
         {
-            var seasonal = _seasonals.FirstOrDefault(s = s.Id == id);
+            var seasonal = _seasonals.FirstOrDefault(s = s.SeasonalId == id);
 
             if (seasonal == null)
             {
                 return NotFound();
             }
             return seasonal;
+        }
+
+        [HttpPost]
+        public ActionResult<Seasonal> CreateSeasonal(Seasonal seasonal)
+        {
+            seasonal.SeasonalId = _seasonals.Max(s => s.SeasonalId) + 1;
+            _seasonals.Add(seasonal);
+            return CreatedAtAction(nameof(GetSeasonalById), new {id = seasonal.SeasonalId}, seasonal);
+        }
+
+        [HttpPut]
+        public ActionResult<Seasonal> UpdateSeasonal(int id, Seasonal updateSeasonal)
+        {
+            var seasonal = _seasonals.Find(s = s.SeasonalId == id);
+
+            if (seasonal == null)
+            {
+                return BadRequest();
+            }
+
+            seasonal.SeasonalName = updateSeasonal.SeasonalName;
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteSeasonal(int id)
+        {
+            var seasonal = _seasonals.Find(s = s.SeasonalId == id);
+            if (seasonal == null)
+            {
+                return NotFound();
+            }
+            _seasonals.Remove(seasonal);
+            return NoContent();
         }
     }
 }
